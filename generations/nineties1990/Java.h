0d27e97eb119a6403ac9994498b77c769ff9808e
sumim <sumi@seagreen.ocn.ne.jp>
2017-05-16 15:37:02 +0900

    SmallTalk -> Smalltalk

--- generations/nineties1990/Java.h
+++ generations/nineties1990/Java.h
@@ -1,9 +1,9 @@
-#include "../seventies1970/SmallTalk.h"
+#include "../seventies1970/Smalltalk.h"
 #include "../eighties1980/ObjectiveC.h"
 #include "../eighties1980/Eiffel.h"
 #include "../eighties1980/CPlusPlus.h"
 #include "../seventies1970/Pascal.h"
 
-class Java : public SmallTalk, public ObjectiveC, public Eiffel, public CPlusPlus, public Pascal {
+class Java : public Smalltalk, public ObjectiveC, public Eiffel, public CPlusPlus, public Pascal {
 
 };
