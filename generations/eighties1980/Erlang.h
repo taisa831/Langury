2d9d458e0d06e001bbc3d6030fb6b65017e0c3ef
sumim <sumi@seagreen.ocn.ne.jp>
2017-05-16 15:34:23 +0900

    SmallTalk -> Smalltalk

--- generations/eighties1980/Erlang.h
+++ generations/eighties1980/Erlang.h
@@ -1,6 +1,6 @@
-#include "../seventies1970/SmallTalk.h"
+#include "../seventies1970/Smalltalk.h"
 #include "../seventies1970/Prolog.h"
 
-class Erlang : public SmallTalk, public Prolog {
+class Erlang : public Smalltalk, public Prolog {
 
 };
