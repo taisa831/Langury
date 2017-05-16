b071d65e7a416b7ce7472f32105428578c7f1ca2
sumim <sumi@seagreen.ocn.ne.jp>
2017-05-16 15:36:14 +0900

    SmallTalk -> Smalltalk

--- newgenerations/tens2010/Dart.h
+++ newgenerations/tens2010/Dart.h
@@ -1,9 +1,9 @@
-#include "../../generations/seventies1970/SmallTalk.h"
+#include "../../generations/seventies1970/Smalltalk.h"
 #include "../../generations/eighties1980/Erlang.h"
 #include "../../generations/nineties1990/Java.h"
 #include "../../generations/nineties1990/JavaScript.h"
 #include "../../newgenerations/twothousand2000/CSharp.h"
 
-class Dart : public SmallTalk, public Erlang, public Java, public JavaScript, public CSharp {
+class Dart : public Smalltalk, public Erlang, public Java, public JavaScript, public CSharp {
 
 };
